4a6831b24bde8f599e4724068fd8efd308aa044b
Osman Yavuz <58117722+OsmanYavuz-web@users.noreply.github.com>
2020-02-23 02:22:02 +0300

    SesVideoBirlestir

---
+++ SesVideoBirlestir/FfmpegSinif.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SesVideoBirlestir
+{
+    public class FfmpegSinif
+    {
+        public static bool CalistirFFMpeg(string arguments)
+        {
+            try
+            {
+                Process process = Process.Start("cmd.exe", $@"/k ffmpeg.exe {arguments}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SesVideoBirlestir/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SesVideoBirlestir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "AAC|*.aac";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = open.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "MP4|*.mp4";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = open.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "MP4|*.mp4";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                string command = $"-i {textBox1.Text} -i {textBox2.Text} -acodec copy -vcodec copy {save.FileName}";
+
+                textBox3.Text = command;
+
+                FfmpegSinif.CalistirFFMpeg(command);
+
+            }
+        }
+    }
+}
